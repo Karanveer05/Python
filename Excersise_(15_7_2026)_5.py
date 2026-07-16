848fb8b926febe9b9a9ed3547dad4639166173d3
Karanveer05 <Karanveernetsquaresoftwares@gmail.com>
2026-07-16 13:22:19 +0530

    adding code lines to read the file

--- Excersise_(15_7_2026)_5.py
+++ Excersise_(15_7_2026)_5.py
@@ -1,6 +1,11 @@
-file=open("attendence.txt","a")
+file=open("attendence.txt","w")
 Number=int(input("Enter the  NO of Students names you want to enter in the list :"))
 for i in range(0,Number):
  Name=str(input(f"Enter the Name of Student {i+1} : "))
  file.write(f"Name of Student {i+1} : {Name}\n")
 file.close()
+Input=input("Enter any key to list the data :")
+print("-"*25)
+with open ("attendence.txt","r") as file:
+ for line in file.readlines():
+     print(line)
